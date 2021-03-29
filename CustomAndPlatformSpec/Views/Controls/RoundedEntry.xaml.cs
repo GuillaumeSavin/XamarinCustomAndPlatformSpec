baa3dfea7383ffa5b321950d91adb0f265ca40aa
GuillaumeSavin <guisav1996@hotmail.fr>
2021-03-30 00:44:40 +0200

    add gesture recognizer

---
+++ CustomAndPlatformSpec/Views/Controls/RoundedEntry.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CustomAndPlatformSpec.Views.Controls
+{
+    public partial class RoundedEntry : ContentView
+    {
+        public RoundedEntry()
+        {
+            InitializeComponent();
+        }
+    }
+}
